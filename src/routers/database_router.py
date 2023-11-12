56afd6494fc0108bfd65799287e995b37f48e66e
PetrAhtimirov <sega20040613@gmail.com>
2023-11-13 00:06:33 +0300

    add some js functions

--- src/routers/database_router.py
+++ src/routers/database_router.py
@@ -1,4 +1,4 @@
-from fastapi import File, UploadFile, APIRouter, Form
+from fastapi import File, UploadFile, APIRouter, Form, Response
 
 from ml.db_creating.create_subject import create_subject
 from ml.db_creating.subject_list import get_subject_list
@@ -11,15 +11,25 @@ router = APIRouter(
 
 
 @router.post("/subject_db_upload")
-async def subject_db_upload(subject: str = Form(...), subject_file: UploadFile = File(...)):
-    file_path = get_path(subject_file.filename)
-    with open(file_path, "wb") as buffer:
-        buffer.write(await subject_file.read())
-    obj = {"name": subject, "themes": [], "path": subject_file.filename}
-    create_subject(obj)
-    return {"status": "ok"}
+async def subject_db_upload(response: Response,
+                            subject: str = Form(...),
+                            subject_file: UploadFile = File(...)):
+    try:
+        file_path = get_path(subject_file.filename)
+        with open(file_path, "wb") as buffer:
+            buffer.write(await subject_file.read())
+        obj = {"name": subject, "themes": [], "path": subject_file.filename}
+        create_subject(obj)
+        return {"status": "ok"}
+    except Exception as e:
+        response.status_code = 500
+        return {"status": "error", "message": str(e)}
 
 
 @router.get("/subject_list")
-async def subject_list():
-    return get_subject_list()
+async def subject_list(response: Response):
+    try:
+        return get_subject_list()
+    except Exception as e:
+        response.status_code = 500
+        return {"status": "error", "message": str(e)}
