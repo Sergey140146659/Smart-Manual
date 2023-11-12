0f1d461f34c5d1a6984c968f86bcc9952a55fedd
Sergey140146659 <sega20040613@gmail.com>
2023-11-12 22:07:13 +0300

    add pdf loader

--- src/routers/database_router.py
+++ src/routers/database_router.py
@@ -1,5 +1,7 @@
 from fastapi import File, UploadFile, APIRouter, Form
 
+from ml.preprocessing_data.Articles_path import get_path
+
 router = APIRouter(
     prefix="/db",
     tags=["DataBase"]
@@ -8,5 +10,7 @@ router = APIRouter(
 
 @router.post("/subject_db_upload")
 async def subject_db_upload(subject: str = Form(...), subject_file: UploadFile = File(...)):
-    print({"filename": subject_file.filename, "subject": subject})
+    file_path = get_path(subject_file.filename)
+    with open(file_path, "wb") as buffer:
+        buffer.write(await subject_file.read())
     return {"filename": subject_file.filename, "subject": subject}
