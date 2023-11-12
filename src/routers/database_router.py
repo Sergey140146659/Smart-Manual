f1995f9803db36e3bab4b4c7b3516fae8c76cf6e
PetrAhtimirov <sega20040613@gmail.com>
2023-11-12 22:46:43 +0300

    set up adding subject db

--- src/routers/database_router.py
+++ src/routers/database_router.py
@@ -1,5 +1,7 @@
 from fastapi import File, UploadFile, APIRouter, Form
 
+from ml.db_creating.create_subject import create_subject
+from ml.db_creating.subject_list import get_subject_list
 from ml.preprocessing_data.Articles_path import get_path
 
 router = APIRouter(
@@ -13,4 +15,11 @@ async def subject_db_upload(subject: str = Form(...), subject_file: UploadFile =
     file_path = get_path(subject_file.filename)
     with open(file_path, "wb") as buffer:
         buffer.write(await subject_file.read())
-    return {"filename": subject_file.filename, "subject": subject}
+    obj = {"name": subject, "themes": [], "path": subject_file.filename}
+    create_subject(obj)
+    return {"status": "ok"}
+
+
+@router.get("/subject_list")
+async def subject_list():
+    return get_subject_list()
