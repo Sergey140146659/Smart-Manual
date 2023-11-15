7e123393c2722b9120fa306e987f397fc95537b4
PetrAhtimirov <46491539+Sergey140146659@users.noreply.github.com>
2023-11-16 01:27:01 +0300

    create wide subjects list

--- src/static/scripts/admin-notes.js
+++ src/static/scripts/admin-notes.js
@@ -40,9 +40,7 @@ window.addEventListener('DOMContentLoaded', () => {
     getWideSubjectList();
 
     function buildWideSubjectListDOM (subjectList) {
-        console.log(subjectList)
         for (subject of subjectList) {
-            console.log(subject);
             createWideSubjectDOM(subject);
         }
     }
@@ -52,15 +50,39 @@ window.addEventListener('DOMContentLoaded', () => {
         wideSubjectsListDOM.innerHTML = '';
     }
 
-    function clearWideSubjectListDOM (subject) {
+    function createWideSubjectDOM (subject) {
+        console.log(subject);
         const wideSubjectsListDOM = document.querySelector('.wide-subject-list');
 
         const newSubject = document.createElement('li');
-        newSubject.classList.add('subject-list__item');
+        newSubject.classList.add('wide-subject-list__item');
         newSubject.innerHTML = `
-            ${subject["name"]}
+            <span>${subject.name}</span>
         `;
-        subjectsListDOM.append(newSubject);
+        let filesList = [];
+        for (theme of subject.themes) {
+            if (filesList.indexOf(theme.file_name) == -1) {
+                filesList.push(theme.file_name);
+            }
+        }
+        for (file of filesList) {
+            newSubject.innerHTML += `
+                <p class="wide-subject-list__subject-file">${file}</p>
+            `;
+            const themesListDOM = document.createElement('ul');
+            for (theme of subject.themes) {
+                if (theme.file_name == file) {
+                    themesListDOM.innerHTML += `
+                        <li class="">
+                            <button type="button" class="subject-list__item__button">${theme.name}</button>
+                        </li>
+                    `;
+                }
+            }
+            newSubject.append(themesListDOM);
+        }
+
+        wideSubjectsListDOM.append(newSubject);
         return newSubject;
     }
 
