3ca7601c2bcc08fff56e7f39279dd7de42d95a80
Sergey140146659 <sega20040613@gmail.com>
2023-11-17 00:15:33 +0300

    delete useles files

--- src/model_testing/making text AI.py
+++ src/model_testing/making text AI.py
@@ -15,7 +15,6 @@ topics = [
     "Дом и семья",
     "Бизнес и финансы",
     "Еда и кулинария",
-    "Образование и саморазвитие",
     "Развлечения и музыка",
     "Религия и духовность",
     "Природа и экология",
@@ -38,56 +37,25 @@ topics = [
     "Кино и телевидение",
     "Литература и поэзия",
     "Путешествия во времени и пространстве",
-    "Гаджеты и электроника"
+    "Гаджеты и электроника", "Спасение вымирающих видов", "Музыкальный театр", "Путешествия во времени и пространстве",
+    "Фотография и видеосъемка", "Огородничество", "Инновации и стартапы",
+    "Роль семьи в формировании личности",
+    "Как правильно выбирать чайную посуду и аксессуары для заваривания чая", "Религиозные традиции и ритуалы",
+    "Искусство и культура", "Здоровье и фитнес",
+    'Путешествия и туризм', "Технологии и наука", "Мода и стиль",
+    "Спорт и активный отдых", "Дом и семья", "Бизнес и финансы", "Еда и кулинария",
+    "Развлечения и музыка",
+    "Религия и духовность", "Природа и экология"
 ]
-
-used_topic = ["Спасение вымирающих видов", "Музыкальный театр", "Путешествия во времени и пространстве",
-              "Фотография и видеосъемка", "Огородничество", "Инновации и стартапы",
-              "Роль семьи в формировании личности",
-              "Как правильно выбирать чайную посуду и аксессуары для заваривания чая", "Религиозные традиции и ритуалы",
-              "Искусство и культура", "Здоровье и фитнес",
-              'Путешествия и туризм', "Технологии и наука", "Мода и стиль",
-              "Спорт и активный отдых", "Дом и семья", "Бизнес и финансы", "Еда и кулинария",
-              "Образование и саморазвитие", "Развлечения и музыка",
-              "Религия и духовность", "Природа и экология"
-              ]
 
 for i in range(len(topics)):
     topic = topics[i]
-    if topic in used_topic:
-        continue
     prompt = f'составь небольшой текст на тему "{topic}"'
-    go = 1
-
-    try:
-        response = g4f.ChatCompletion.create(
-            model=g4f.models.gpt_4,
-            messages=[{"role": "user", "content": prompt}],
-        )
-        response = str(response)
-        with open('texts_gpt4.txt', 'a') as file:
-            file.write(response)
-        print(f'{topic}:\n{response}')
-        used_topic.append(topic)
-    except:
-        new_prompt = f'составь небольшой текст по теме "{topic}"'
-        time.sleep(30)
-        try:
-            response = g4f.ChatCompletion.create(
-                model=g4f.models.gpt_4,
-                messages=[{"role": "user", "content": prompt}],
-            )
-            response = str(response)
-            with open('texts_gpt4.txt', 'a') as file:
-                file.write(response)
-            print(f'{topic}:\n{response}')
-            used_topic.append(topic)
-        except:
-            continue
-
-print("used:", used_topic)
-unused = []
-for i in topics:
-    if i not in used_topic:
-        unused.append(i)
-print("unused:", unused)
+    response = g4f.ChatCompletion.create(
+        model=g4f.models.gpt_4,
+        messages=[{"role": "user", "content": prompt}],
+    )
+    response = str(response)
+    with open('texts_gpt4.txt', 'a') as file:
+        file.write(response)
+    print(f'{topic}:\n{response}')
